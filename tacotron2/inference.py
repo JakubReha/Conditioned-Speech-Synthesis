a43607db6b7cc8fe8ace199cd627e073241484f6
JakubReha <kubareha107@gmail.com>
2022-05-13 00:25:10 +0200

    waveglow inference

--- tacotron2/inference.py
+++ tacotron2/inference.py
@@ -9,12 +9,16 @@ from train import load_model
 from text import text_to_sequence
 from denoiser import Denoiser
 import librosa
+import soundfile as sf
+from convert_model import update_model
 
 def plot_data(data, figsize=(16, 4)):
     fig, axes = plt.subplots(1, len(data), figsize=figsize)
     for i in range(len(data)):
-        axes[i].imshow(data[i], aspect='auto', origin='bottom',
+        axes[i].imshow(data[i], aspect='auto',
                        interpolation='none')
+    plt.savefig('foo.png')
+    plt.show()
 
 hparams = create_hparams()
 hparams.sampling_rate = 22050
@@ -24,15 +28,17 @@ model = load_model(hparams)
 model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
 _ = model.cuda().eval().half()
 
-waveglow_path = 'waveglow_256channels.pt'
+waveglow_path = 'waveglow_256channels_new.pt'
 waveglow = torch.load(waveglow_path)['model']
+waveglow = waveglow.remove_weightnorm(waveglow)
+
 waveglow.cuda().eval().half()
 for k in waveglow.convinv:
     k.float()
 denoiser = Denoiser(waveglow)
 
 
-text = "Waveglow is really awesome!"
+text = "Fuck yeah! It finally works!"
 sequence = np.array(text_to_sequence(text, ['english_cleaners']))[None, :]
 sequence = torch.autograd.Variable(
     torch.from_numpy(sequence)).cuda().long()
@@ -42,11 +48,10 @@ plot_data((mel_outputs.float().data.cpu().numpy()[0],
            mel_outputs_postnet.float().data.cpu().numpy()[0],
            alignments.float().data.cpu().numpy()[0].T))
 
-
 with torch.no_grad():
     audio = waveglow.infer(mel_outputs_postnet, sigma=0.666)
-librosa.output.write_wav("demo.wav", audio[0].data.cpu().numpy(), sr=hparams.sampling_rate)
+sf.write("demo.wav", audio[0].data.cpu().numpy().astype(np.float32), hparams.sampling_rate)
 
 audio_denoised = denoiser(audio, strength=0.01)[:, 0]
-librosa.output.write_wav("demo_denoised.wav", audio_denoised.cpu().numpy(), sr=hparams.sampling_rate)
+sf.write("demo_denoised.wav", audio_denoised.squeeze().cpu().numpy().astype(np.float32), hparams.sampling_rate)
 
